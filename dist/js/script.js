8ea35878b05fb57a670533355cb479df6ae10143
Naufal05R <naufalrabbaniishere@gmail.com>
2022-12-10 09:54:50 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,7 +2,6 @@ const navbar = document.getElementsByTagName('nav')[0];
 const toggle = document.getElementsByClassName('navbar-toggler')[0];
 
 window.addEventListener('scroll', function () {
-  console.log(window.scrollY);
   if (window.scrollY > 1) {
     navbar.classList.replace('bg-transparent', 'nav-color');
   } else if (this.window.scrollY <= 0) {
@@ -13,7 +12,7 @@ window.addEventListener('scroll', function () {
 toggle.addEventListener('click', function () {
   if (toggle.ariaExpanded === 'true') {
     navbar.classList.replace('bg-transparent', 'nav-color');
-  } else if (toggle.ariaExpanded === 'false') {
+  } else if (window.scrollY <= 0) {
     navbar.classList.replace('nav-color', 'bg-transparent');
   }
 });
